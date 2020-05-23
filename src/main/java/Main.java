3b56a7a7920830ef0a6414e78b5202d10d4c1a2c
Alex <mogildea.alexandru@yahoo.com>
2020-05-23 23:01:38 +0300

    String manipulation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 //        missingChar("Alex", 3);
-
+//        frontBack("Alex");
 
 
     }
@@ -72,7 +72,27 @@ public class Main {
     }
 
     // Given a string, return a new string where the first and last chars have been exchanged.
+    public static String frontBack(String str) {
+        if(str.length() <=1) return str;
 
+        String mid = str.substring(1, str.length()-1);
+
+        System.out.println(str.charAt(str.length()-1) + mid + str.charAt(0));
+        return "";
+    }
+
+    //Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
+    public String front3(String str) {
+
+        String front;
+
+        if (str.length() >=3) {
+            front = str.substring(0,3);
+        } else {
+            front = str;
+        }
+        return front + front + front;
+    }
 
 
 
